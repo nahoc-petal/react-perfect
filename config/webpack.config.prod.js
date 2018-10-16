f1f2f03683f9230d15b4e1a83bf9610e06e0c587
nahoc-petal <ccarpentier@petalmd.com>
2018-10-16 15:46:55 -0400

    added css modules support

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -197,8 +197,9 @@ module.exports = {
                       loader: require.resolve('css-loader'),
                       options: {
                         importLoaders: 1,
+                        modules: true,
                         minimize: true,
-                        sourceMap: shouldUseSourceMap,
+                        sourceMap: true, 
                       },
                     },
                     {
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,12 @@
 import * as React from 'react';
-import './App.css';
-
-import logo from './logo.svg';
+import styles from './App.module.css';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
+      <div className={styles.app}>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Perfect React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
